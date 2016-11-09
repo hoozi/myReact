c3f36e43b2f760d3f6d0f65f9128777847d11a8a
AngryPanda <287036406@qq.com>
2016-11-09 16:50:08 +0800

    createComponent

---
+++ createComponent/app.js
@@ -0,0 +1,49 @@
+/**
+ * 创建React component的两种方法
+ * React.createClass 以及 ES6 class Component extends React.Component
+ */
+/*const test = function(){
+       return { 
+           say() {
+            alert("Hello "+this.props.value);
+        }
+    }
+}
+
+const Component = React.createClass({
+    mixins:[test()], //抽离公共部分
+    getDefaultProps() {
+        return {
+            value:"world"
+        }
+    },
+    render() {
+        return <div onClick={this.say}>Hello {this.props.value}!</div>
+    }
+})*/
+
+/**
+ * 用ES6构造react Component
+ */
+class Component extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    
+    say() {
+        alert("Hello "+this.props.value)
+    }
+
+    render() {
+        return <div onClick={this.say.bind(this)}>Hello {this.props.value}!</div>
+    }
+}
+Component.defaultProps = {
+        value: "world"  
+}
+
+ReactDOM.render(
+    <Component/>,
+    document.getElementById("container")
+)
